31a4358a0f3e1e56ba7ce72f7deed5802d6e2d6b
creative_prowess <support@prowess.io>
2025-06-20 15:47:23 -0800

    Account menu push

--- components/AccountMenu.js
+++ components/AccountMenu.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function AccountMenu() {
   return (
-    <div className="space-x-4">
+    <div className="hidden md:flex flex-1 space-x-4">
       <Link href="/account" className="hover:underline">My Account</Link>
       <Link href="/checkout" className="hover:underline">Checkout</Link>
       <Link href="/login" className="hover:underline">Login/Register</Link>
