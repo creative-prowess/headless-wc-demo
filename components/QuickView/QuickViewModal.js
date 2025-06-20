56fc24f5559f520602bcb51b593b4250091f3843
creative_prowess <support@prowess.io>
2025-06-20 15:14:44 -0800

    Quick view changes

--- components/QuickView/QuickViewModal.js
+++ components/QuickView/QuickViewModal.js
@@ -18,7 +18,7 @@ export default function QuickViewModal({ isOpen, onClose, product }) {
   useEffect(() => {
     if (!product?.variations?.nodes?.length) return
     const init = {}
-    product.variations.nodes[0].attributes?.nodes.forEach(({ name, value }) => {
+product?.variations?.nodes?.[0]?.attributes?.nodes?.forEach(({ name, value }) => {
       init[name] = value
     })
     setSelectedAttributes(init)
@@ -26,10 +26,9 @@ export default function QuickViewModal({ isOpen, onClose, product }) {
 
   // Update selectedVariation when attributes change
   useEffect(() => {
-    if (!product?.variations?.nodes?.length) return
-    const match = product.variations.nodes.find((v) =>
-      v.attributes.nodes.every((attr) => selectedAttributes[attr.name] === attr.value)
-    )
+const match = product?.variations?.nodes?.find((v) =>
+  v.attributes?.nodes?.every((attr) => selectedAttributes[attr.name] === attr.value)
+)
     setSelectedVariation(match || null)
   }, [selectedAttributes])
 
