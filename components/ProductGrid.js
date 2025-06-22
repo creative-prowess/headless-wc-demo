2a22976b6e3d36b7256448541eb32a8bd6c7111b
creative_prowess <support@prowess.io>
2025-06-22 12:05:24 -0800

    Grid Update

--- components/ProductGrid.js
+++ components/ProductGrid.js
@@ -11,7 +11,7 @@ export default function ProductGrid({ products = [], loading = false }) {
 
   if (loading) {
     return (
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 ">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 p-4">
         {Array.from({ length: 8 }).map((_, i) => (
           <div
             key={i}
