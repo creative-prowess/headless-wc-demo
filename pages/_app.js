410864be25f4e8deaec86114cd4eb0ddfa2d27a5
creative_prowess <support@prowess.io>
2025-06-25 03:10:27 -0800

    APP Commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,20 +7,28 @@ import { CartProvider } from '../context/CartContext'
 import { ToastProvider } from '../context/ToastContext'
 import { WishlistProvider } from '@/context/WishlistContext'
 import { CompareProvider } from '@/context/CompareContext'
+import { CookieConsentProvider } from '@/context/CookieConsentContext'
+import CookieConsentBanner from '@/components/CookieConsentBanner'
+import CookieSettingsModal from '@/components/CookieSettingsModal'
+
 
 export default function App({ Component, pageProps }) {
 
   return (
 <ApolloProvider client={client}>
+   <CookieConsentProvider>
   <CartProvider>
     <WishlistProvider>
       <CompareProvider>
         <ToastProvider>
           <Component {...pageProps} />
+          <CookieConsentBanner />
+      <CookieSettingsModal />
         </ToastProvider>
       </CompareProvider>
     </WishlistProvider>
   </CartProvider>
+  </CookieConsentProvider>
 </ApolloProvider>
   )
 }
