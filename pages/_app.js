88a876f10945c26d6b67c71c4a4bb241ac566f9e
creative_prowess <support@prowess.io>
2025-06-24 18:55:24 -0800

    Update APP

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
+import "keen-slider/keen-slider.min.css"
 import { ApolloProvider } from '@apollo/client'
 import client from '../lib/apolloClient'
+import { useEffect } from 'react'
 import { CartProvider } from '../context/CartContext'
 import { ToastProvider } from '../context/ToastContext'
 import { WishlistProvider } from '@/context/WishlistContext'
 import { CompareProvider } from '@/context/CompareContext'
+
 export default function App({ Component, pageProps }) {
+
   return (
 <ApolloProvider client={client}>
   <CartProvider>
